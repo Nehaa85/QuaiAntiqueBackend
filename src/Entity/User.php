678429e801fe982ec226fc2641288a8b20ae59e2
Donovan <schumi.dono@gmail.com>
2024-07-12 16:35:54 +0200

    Ajout des DataFixtures + Modifications User.php

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -40,6 +40,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 255)]
     private ?string $apiToken = null;
 
+    #[ORM\Column(length: 32)]
+    private ?string $firstName = null;
+
+    #[ORM\Column(length: 64)]
+    private ?string $lastName = null;
+
+    #[ORM\Column(type: 'smallint', nullable: true)]
+    private ?int $guestNumber = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $allergy = null;
+
     /** @throws \Exception */
     public function __construct()
     {
@@ -60,6 +72,50 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         $this->email = $email;
 
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): static
+    {
+        $this->firstName = $firstName;
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): static
+    {
+        $this->lastName = $lastName;
+        return $this;
+    }
+
+    public function getGuestNumber(): ?int
+    {
+        return $this->guestNumber;
+    }
+
+    public function setGuestNumber(?int $guestNumber): static
+    {
+        $this->guestNumber = $guestNumber;
+        return $this;
+    }
+
+    public function getAllergy(): ?string
+    {
+        return $this->allergy;
+    }
+
+    public function setAllergy(?string $allergy): static
+    {
+        $this->allergy = $allergy;
         return $this;
     }
 
